69fb5d3ea66d3e16f18d3b9aac0a6618c8bda58f
haikallf <haikallzrd@gmail.com>
2022-07-13 18:05:28 +0700

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import Organogram from './pages/Organogram';
 
-function App() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
-}
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/organogram" element={<Organogram />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/pages/Organogram.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Organogram: React.FC = () => {
+  return <div>Organogram</div>;
+};
+
+export default Organogram;
