56baa4d7280c50af42ca0ae9d7d2c40e000df408
haikallf <haikallzrd@gmail.com>
2022-07-16 05:58:04 +0700

    add new constant

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
     extend: {
       colors: {
         primaryYellow: '#FFEBB0',
+        secondaryYellow: '#FFC176',
+        tertiaryYellow: '#FFE779',
+        quarternaryYellow: '#F5ECCB',
+        primaryCream: '#FFFCF2',
+        primaryOrange: '#FFA06F',
+        secondaryOrange: '#FF7D4B',
+        primaryBlack: '#171133',
+        grey: '#D9D9D9',
+        primaryGreen: '#2F4250',
+        secondaryGreen: '#4F6266',
       },
       fontSize: {
         heading: '48px',
