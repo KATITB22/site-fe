ddac32f3b1c3f2d9d6bd0dcecceadadf3332116c
fikrikhoironn <fikrikf470@gmail.com>
2022-07-16 22:20:49 +0700

    feat: add word art kesekjenan

--- src/containers/OrganogramSection.tsx
+++ src/containers/OrganogramSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardRole from '../components/CardRole';
+import RoleText from '../components/RoleText';
 
 const OrganogramSection: React.FC = () => {
   return (
@@ -10,6 +11,11 @@ const OrganogramSection: React.FC = () => {
         name="Lorep Ipsum"
         jurusan="IF 20"
       />
+      <div className="grid grid-cols-3 gap-3 py-14">
+        <div className="col-start-auto bg-[#F5ECCB] w-100 h-px mt-4 "></div>
+        <RoleText className="text-heading text-center">Kesekjenan</RoleText>
+        <div className="col-end-auto bg-[#F5ECCB] w-100 h-px mt-4"></div>
+      </div>
     </div>
   );
 };
