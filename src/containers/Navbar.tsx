1813ccf5e9700dc55cb5316f47fbcdf2746ad05a
rafirm29 <13519154@std.stei.itb.ac.id>
2022-07-19 22:43:31 +0700

    feat: add navbar component

---
+++ src/containers/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavDropdown from '../components/navbar/NavDropdown';
+import NavIcon from '../components/navbar/NavIcon';
+import { NavItemProps } from '../components/navbar/NavItem';
+
+interface NavbarProps {
+  children:
+    | React.ReactElement<NavItemProps>
+    | React.ReactElement<NavItemProps>[];
+}
+
+const Navbar: React.FC<NavbarProps> = ({ children }) => {
+  return (
+    <nav className="mt-4 px-8 bg-tertiaryYellow flex">
+      <div className="mr-auto">
+        <NavIcon />
+      </div>
+      <ul className="gap-16 hidden md:flex">{children}</ul>
+      <div className="block md:hidden">
+        <NavDropdown>{children}</NavDropdown>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import NavItem from '../components/navbar/NavItem';
 import AplikasiLainnya from '../containers/AplikasiLainnya';
 import Footer from '../containers/Footer';
+import Navbar from '../containers/Navbar';
 
 export const Home: React.FC = () => {
   return (
     <div>
+      <Navbar>
+        <NavItem name="Organogram KAT ITB" to="organogram-kat" />
+        <NavItem name="Organogram IT" to="divisi-it" />
+        <NavItem name="Aplikasi Lainnya" to="/" />
+      </Navbar>
       <AplikasiLainnya />
       <Footer />
     </div>
