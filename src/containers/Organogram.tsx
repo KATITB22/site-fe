37184e2399ceedd71bde427914bbce4eab1007bc
raniadf <16520006@mahasiswa.itb.ac.id>
2022-07-20 00:33:38 +0700

    add new breakpoint & fix responsive

--- src/containers/Organogram.tsx
+++ src/containers/Organogram.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import OrganogramWaveBottom from '../components/OrganogramWaveBottom';
 import OrganogramWaveTop from '../components/OrganogramWaveTop';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Title from '../components/Title';
 import TopTitle from '../components/TopTitle';
 import Texts from '../components/Texts';
@@ -31,14 +31,16 @@ const Organogram: React.FC = () => {
   return (
     <div className="relative w-[100vw]">
       <OrganogramWaveTop width={screenSize.dynamicWidth} />
-      <section className='absolute flex items-center md:flex-row-reverse flex-wrap w-full h-auto xs:px-20 sm:px-36 xs:top-24 sm:top-32 md:top-40 lg:top-36 xl:top-28'>
-        <section className='relative md:w-1/4 xl:w-1/3 h-0 xs:invisible lg:h-[220px] lg:visible lg:ml-0 lg:top-0'>
-            <TopTitle className="absolute text-right top-0 xl:mr-7">
-                Organo <br/> gram
-            </TopTitle>
-            <VStock className='absolute h-[220px] w-auto top-5 xl:ml-28'/>
+      <section className='absolute flex items-center justify-center lg:flex-row-reverse flex-wrap w-full h-auto xs:px-20 sm:px-36 xs:top-28 sm:top-32 md:top-40 lg:top-36 xl:top-32 xxl:top-30'>
+        <section className='flex justify-center lg:w-1/4 xl:w-1/3'>
+          <section className='relative md:w-[220px] h-0 xs:invisible lg:h-[220px] lg:visible lg:top-0'>
+              <TopTitle className="absolute text-right top-0 xl:mr-7">
+                  Organo <br/> gram
+              </TopTitle>
+              <VStock className='absolute h-[220px] w-auto top-5 xl:ml-0'/>
+          </section>
         </section>
-        <section className='flex flex-col space-y-2 w-3/4 xl:w-2/3'>
+        <section className='flex flex-col space-y-2 lg:w-3/4 xl:w-2/3'>
           <Title className="relative">
                 Organogram
           </Title>
@@ -55,6 +57,7 @@ const Organogram: React.FC = () => {
           </section>
         </section> 
       </section>
+      <section className='w-full bg-primaryYellow h-0 xl:h-5 xxl:h-28 xs:invisible xl:visible' />
       <OrganogramWaveBottom width={screenSize.dynamicWidth} />
     </div>
   );
