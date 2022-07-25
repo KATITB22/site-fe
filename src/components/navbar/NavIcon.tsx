ed1cb0e1f516baa83388db7d9eaef5f70b30b0f4
haikallf <haikallzrd@gmail.com>
2022-07-26 02:23:55 +0700

    dynamic and sticky navbar

--- src/components/navbar/NavIcon.tsx
+++ src/components/navbar/NavIcon.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const NavIcon: React.FC = () => {
+interface NavIconProps {
+  darkMode: boolean;
+}
+
+const NavIcon: React.FC<NavIconProps> = ({ darkMode }) => {
   return (
     <div className="pt-2 pb-2.5">
-      <div className="w-10 h-10 rounded-full bg-grey absolute top-2" />
-      <div className="ml-12 font-magilio">KAT ITB 2022</div>
+      <div className="w-10 h-10 rounded-full bg-grey absolute -top-2" />
+      <div
+        className={`ml-12 font-magilio ${
+          darkMode ? 'text-quarternaryYellow' : 'text-primaryBlack'
+        }`}
+      >
+        KAT ITB 2022
+      </div>
     </div>
   );
 };
--- src/components/navbar/NavItem.tsx
+++ src/components/navbar/NavItem.tsx
@@ -4,12 +4,15 @@ import { Link, To } from 'react-router-dom';
 export interface NavItemProps {
   name: string;
   to: string;
+  darkMode: boolean;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ name, to }) => {
+const NavItem: React.FC<NavItemProps> = ({ name, to, darkMode }) => {
   return (
     <li className="pt-2 pb-2.5 font-alegreyasans">
-      <Link to={to}>{name}</Link>
+      <Link style={{ color: `${darkMode ? '#F5ECCB' : '#171133'}` }} to={to}>
+        {name}
+      </Link>
     </li>
   );
 };
