16d5c9d2920bd091e9e6a12c15f1c237f83b2613
haikallf <haikallzrd@gmail.com>
2022-07-26 02:01:31 +0700

    fix typo

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Body from './Body';
 import Title from './Title';
 
-
 interface AgendaProps {
   text?: string;
   date?: Date;
@@ -10,15 +9,22 @@ interface AgendaProps {
 
 const Agenda: React.FC<AgendaProps> = ({ text, date }) => {
   return (
-  <div className='flex items-center'>
-    <svg height="85" width="80">
-      <circle cx="50" cy="30" r="15" stroke="#FF7D4B" stroke-width="2.5" fill="transparent" />
-    </svg> 
-    <div className='float-left'>
-      <Title className='text-[35px] p-0 m-0 leading-none'>Dikpus</Title>
-      <Body className='text-[17px] p-0 m-0 leading-tight'>14 Jul 2022</Body>
+    <div className="flex items-center">
+      <svg height="85" width="80">
+        <circle
+          cx="50"
+          cy="30"
+          r="15"
+          stroke="#FF7D4B"
+          strokeWidth="2.5"
+          fill="transparent"
+        />
+      </svg>
+      <div className="float-left">
+        <Title className="text-[35px] p-0 m-0 leading-none">Dikpus</Title>
+        <Body className="text-[17px] p-0 m-0 leading-tight">14 Jul 2022</Body>
+      </div>
     </div>
-  </div>
   );
 };
 
--- src/components/TimelineBar.tsx
+++ src/components/TimelineBar.tsx
@@ -4,13 +4,13 @@ import HorizontalLine from './HorizontalLine';
 import VerticalLine from './VerticalLine';
 
 interface TimelineBarProps {
-    text?: string;
-    date?: Date;
-  }
-  
-  const TimelineBar: React.FC<TimelineBarProps> = ({ text, date }) => {
-    return (
-    <div className='justify-center md:flex bg-primaryYellow items-center xs:px-20 sm:px-36 pt-14'>
+  text?: string;
+  date?: Date;
+}
+
+const TimelineBar: React.FC<TimelineBarProps> = ({ text, date }) => {
+  return (
+    <div className="justify-center md:flex bg-primaryYellow items-center xs:px-20 sm:px-36 pt-14">
       <Agenda></Agenda>
       <HorizontalLine></HorizontalLine>
       <VerticalLine></VerticalLine>
@@ -19,7 +19,7 @@ interface TimelineBarProps {
       <VerticalLine></VerticalLine>
       <Agenda></Agenda>
     </div>
-    );
-  };
-  
-  export default TimelineBar;
+  );
+};
+
+export default TimelineBar;
