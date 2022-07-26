b272644602b64e3354e489b86f3c5bd3710a4a8c
haikallf <haikallzrd@gmail.com>
2022-07-26 22:56:20 +0700

    navigate navbar logo to home

--- src/components/navbar/NavIcon.tsx
+++ src/components/navbar/NavIcon.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface NavIconProps {
   darkMode: boolean;
 }
 
 const NavIcon: React.FC<NavIconProps> = ({ darkMode }) => {
+  const navigate = useNavigate();
   return (
-    <div className="pt-2 pb-2.5">
+    <div
+      onClick={() => navigate('/', { replace: true })}
+      className="pt-2 pb-2.5 cursor-pointer"
+    >
       <div className="w-10 h-10 rounded-full bg-grey absolute -top-2" />
       <div
         className={`ml-12 font-magilio ${
