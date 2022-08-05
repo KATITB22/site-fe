74d58cdc38850a9e54754ccaf986e28ff197be2e
arsaizdihar <arsadihar@gmail.com>
2022-08-05 14:30:55 +0700

    Make navbar hidden at not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { MotionConfig } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { Home } from './pages/Home';
+import NavItem from './components/navbar/NavItem';
+import Navbar from './containers/Navbar';
 import DivisiIT from './pages/DivisiIT';
+import { Home } from './pages/Home';
 import OrganogramKAT from './pages/OrganogramKAT';
 import PageNotFound from './pages/PageNotFound';
-import Navbar from './containers/Navbar';
-import NavItem from './components/navbar/NavItem';
 
 const App: React.FC = () => {
   const location = useLocation();
   const [darkMode, setDarkMode] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
 
   useEffect(() => {
     location.pathname === '/organogram-kat'
@@ -19,19 +20,24 @@ const App: React.FC = () => {
   }, [location.pathname]);
 
   return (
-    <MotionConfig reducedMotion='user'>
-      <Navbar darkMode={darkMode}>
-        <NavItem
-          darkMode={darkMode}
-          name="Organogram KAT ITB"
-          to="organogram-kat"
-        />
-        <NavItem darkMode={darkMode} name="Organogram IT" to="divisi-it" />
-        <NavItem darkMode={darkMode} name="Aplikasi Lainnya" to="/" />
-      </Navbar>
+    <MotionConfig reducedMotion="user">
+      {showNavbar && (
+        <Navbar darkMode={darkMode}>
+          <NavItem
+            darkMode={darkMode}
+            name="Organogram KAT ITB"
+            to="organogram-kat"
+          />
+          <NavItem darkMode={darkMode} name="Organogram IT" to="divisi-it" />
+          <NavItem darkMode={darkMode} name="Aplikasi Lainnya" to="/" />
+        </Navbar>
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route
+          path="*"
+          element={<PageNotFound setShowNavbar={setShowNavbar} />}
+        />
         <Route path="/organogram-kat" element={<OrganogramKAT />} />
         <Route path="/divisi-it" element={<DivisiIT />} />
       </Routes>
