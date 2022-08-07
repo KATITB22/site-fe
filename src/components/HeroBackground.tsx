9dcb1fe8e3e6d45f50b9b5cffb3b6494f0d894ea
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-08-08 00:18:29 +0700

    Fix hero background position

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -8,14 +8,14 @@ import heroRight from '../assets/img/vistock/landing/hero/right.png';
 const HeroBackground: React.FC = () => {
     return (
         <>
-            <div className={`flex absolute top-0 left-0 min-h-full z-[-1]`}>
-                <div className={`flex left-0 w-full xl:h-[660px] 2xl:h-full h-[600px]`}>
-                    <img src={heroLeft} className={`object-top object-cover`} alt="heroLeft" />
+            <div className={`flex absolute top-0 left-0 min-h-full -z-10`}>
+                <div className={`flex left-0 h-[630px] w-[1000px]`}>
+                    <img src={heroLeft} className={`object-left object-cover`} alt="heroLeft" />
                 </div>
             </div>
-            <div className={`flex absolute top-0 right-0 min-h-full z-[-2]`}>
-                <div className={`flex right-0 w-full sm:h-[750px] h-[600px]`}>
-                    <img src={heroRight} className={`object-top object-cover`} alt="heroRight" />
+            <div className={`flex absolute top-0 right-0 min-h-full -z-20`}>
+                <div className={`flex right-0 w-[1000px] sm:h-[750px] h-[600px]`}>
+                    <img src={heroRight} className={`object-right object-cover`} alt="heroRight" />
                 </div>
             </div>
         </>
