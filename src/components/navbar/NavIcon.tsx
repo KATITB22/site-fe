58f72d38070abdf2ea565ab4d32565f7c91895dd
haikallf <haikallzrd@gmail.com>
2022-08-12 01:55:07 +0700

    add oskm logo, remove redundant navbar, add sponsors and copyright label

--- src/components/navbar/NavIcon.tsx
+++ src/components/navbar/NavIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import OSKM from '../../assets/img/logo/LogoOnly.png';
 
 const NavIcon: React.FC = () => {
   const navigate = useNavigate();
@@ -8,7 +9,9 @@ const NavIcon: React.FC = () => {
       onClick={() => navigate('/', { replace: true })}
       className="pt-2 pb-2.5 cursor-pointer"
     >
-      <div className="w-10 h-10 rounded-full bg-grey absolute -top-2" />
+      <div className="w-10 h-10 rounded-full absolute -top-2 flex items-center justify-center">
+        <img src={OSKM} className="w-8" alt="Logo Only" />
+      </div>
       <div className="ml-12 font-magilio text-primaryBlack">KAT ITB 2022</div>
     </div>
   );
--- src/pages/OrganogramKAT.tsx
+++ src/pages/OrganogramKAT.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Footer from '../containers/Footer';
 import OrganogramSection from '../containers/OrganogramSection';
-import Navbar from '../containers/Navbar';
-import NavItem from '../components/navbar/NavItem';
 
 const OrganogramKAT: React.FC = () => {
   return (
