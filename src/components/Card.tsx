7f2e889b9b537ff1cd4c17f95580a8494c394cbb
haikallf <haikallzrd@gmail.com>
2022-08-12 23:52:30 +0700

    add applist in aplikasi lainnya

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,29 @@
+import { Moment } from 'moment';
 import React from 'react';
 
 interface CardProps {
-  text?: string;
-  link?: string;
+  name: string;
+  src: string;
+  date: Date | Moment;
+  url: string;
 }
 
-const Card: React.FC<CardProps> = ({ text, link }) => {
-  return <div className="bg-grey h-[300px] w-[200px] text-white">{text}</div>;
+const Card: React.FC<CardProps> = ({ name, src, date, url }) => {
+  // return <div className="bg-grey h-[300px] w-[200px] text-white">{text}</div>;
+  const handleClick = () => {
+    window.location.href = url;
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white h-[300px] w-[200px] text-white rounded-[10px] cursor-pointer hover:scale-[1.02] 
+      transition-transform"
+      style={{ boxShadow: '12px 11px 19px -3px rgba(0,0,0,0.75)' }}
+    >
+      <img className="rounded-[10px]" src={src} alt={name} />
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -4,6 +4,7 @@ import Slick, { Settings } from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Card from './Card';
+import { aplikasiLainnya } from '../utils/aplikasiLainnya';
 
 const slickSettings: Settings = {
   lazyLoad: 'ondemand',
@@ -41,14 +42,15 @@ const CarouselCard: React.FC = () => {
   return (
     <div className="m:w-[600px] lg:w-[800px] xl:w-[1000px] content-center my-0 mx-auto pt-10 flex">
       <Slick {...slickSettings}>
-        <Card text="1" />
-        <Card text="2" />
-        <Card text="3" />
-        <Card text="4" />
-        <Card text="1" />
-        <Card text="2" />
-        <Card text="3" />
-        <Card text="4" />
+        {aplikasiLainnya.map((app, idx) => (
+          <Card
+            key={idx}
+            name={app.name}
+            src={app.src}
+            date={app.date}
+            url={app.url}
+          />
+        ))}
       </Slick>
     </div>
   );
