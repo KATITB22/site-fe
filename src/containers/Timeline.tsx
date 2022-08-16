8faacd612caf6706bfe3bd3ab8debf86a567c88a
haikallf <haikallzrd@gmail.com>
2022-08-16 15:07:32 +0700

    fix current date in timeline section

--- src/containers/Timeline.tsx
+++ src/containers/Timeline.tsx
@@ -28,9 +28,6 @@ const Timeline: React.FC = () => {
   return (
     <div className="relative z-[3] -mt-56">
       <TimelineWaveTop width={screenSize.dynamicWidth} />
-      {/* <OverlapTitle className="xs:px-20 md:px-32 lg:px-32 xl:px-40 xxl:px-80 3xl:px-96 absolute top-48 h-0">
-        imeine <br /> &emsp; KAT ITB 2022
-      </OverlapTitle> */}
       <MotionTitle
         className="absolute top-24 xs:px-20 md:px-32 lg:px-32 xl:px-40 xxl:px-80 3xl:px-96"
         {...getTransition('left')}
