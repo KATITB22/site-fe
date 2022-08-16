a8b2f9aad334c4a98ff99759e5a62fe10e1d4c69
jasonk19 <13520080@std.stei.itb.ac.id>
2022-08-16 13:09:15 +0700

    fix: vistock after fix scaling

--- src/components/OrganogramGraphics.tsx
+++ src/components/OrganogramGraphics.tsx
@@ -11,12 +11,12 @@ const OrganogramGraphics: React.FC = () => {
   return (
     <>
       <VStock
-        className="absolute xs:invisible lg:visible xl:-left-32 lg:-left-44 3xl:-left-18 lg:top-[3500px] xl:top-[2500px] xxl:top-[1900px] 3xl:top-[1600px] xxl:w-[450px] xl:w-[450px] lg:w-[310px] 4xl:top-[1600px] 5xl:top-[1400px]"
+        className="absolute xs:invisible lg:visible xl:-left-32 lg:-left-44 3xl:-left-18 lg:top-[3500px] xl:top-[2500px] xxl:top-[2400px] 3xl:top-[2150px] xxl:w-[450px] xl:w-[450px] lg:w-[310px] 4xl:top-[2200px] 5xl:top-[1800px]"
         src={pohon3}
         animation="bottom"
       />
       <VStock
-        className="absolute xs:invisible lg:visible xl:-top-[160px] lg:top-[420px] -right-8 xxl:w-[450px] xl:w-[350px] lg:w-[320px]"
+        className="absolute xs:invisible lg:visible xl:top-40 lg:top-[420px] -right-8 xxl:w-[450px] xl:w-[350px] lg:w-[320px]"
         src={pohon2}
         animation="bottom"
       />
@@ -27,12 +27,12 @@ const OrganogramGraphics: React.FC = () => {
         mirror
       />
       <VStock
-        className="absolute xs:invisible lg:visible xl:top-100 lg:top-[2250px] xl:top-[2000px] 3xl:top-[1700px] 5xl:top-[1400px] 3xl:top-[2000px] -right-10 xxl:w-[450px] xl:w-[300px] lg:w-[320px]"
+        className="absolute xs:invisible lg:visible xl:top-100 lg:top-[2250px] xl:top-[3000px] xxl:top-[2400px] 3xl:top-[2000px] 5xl:top-[1800px] -right-10 xxl:w-[450px] xl:w-[300px] lg:w-[320px]"
         src={pohon2}
         animation="bottom"
       />
       <VStock
-        className="absolute xs:invisible lg:visible xl:visible xxl:visible -right-8 xxl:top-[3800px] xl:top-[4300px] lg:top-[5100px] 3xl:top-[3200px] xxl:w-[450px] xl:w-[300px] lg:w-[310px] 4xl:top-[3100px] 5xl:top-[2600px]"
+        className="absolute xs:invisible lg:visible xl:visible xxl:visible -right-8 xxl:top-[3800px] xl:top-[4300px] lg:top-[5100px] xl:top-[5000px] xxl:top-[4200px] 3xl:top-[3600px] xxl:w-[450px] xl:w-[300px] lg:w-[310px] 4xl:top-[3200px] 5xl:top-[2900px]"
         src={pohon2}
         animation="bottom"
       />
